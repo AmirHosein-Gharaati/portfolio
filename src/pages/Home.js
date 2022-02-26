4dcf93d7e7b7705159fe14bca5af08c305f9d580
amir <amirgh1380@gmail.com>
2022-02-26 11:02:12 +0330

    main section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,24 @@ const Home = () => {
   const myImage = require("../assets/images/me.png");
 
   return (
-    <section className="home-section">
-      <div className="container">
-        <div className="row align-center">
-          <div className="home__text">
-            <p>Hello, I'm</p>
-            <h1>Amirhosein Gharaati</h1>
-            <h2>Frontend Developer</h2>
-          </div>
-          <div className="home__img">
-            <div className="img-box">
-              <img src={myImage} alt="me" />
+    <div className="main">
+      <section className="home__section align-center">
+        <div className="container">
+          <div className="row align-center">
+            <div className="home__text">
+              <p>Hello, I'm</p>
+              <h1>Amirhosein Gharaati</h1>
+              <h2>Frontend Developer</h2>
+            </div>
+            <div className="home__img">
+              <div className="img-box">
+                <img src={myImage} alt="me" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
