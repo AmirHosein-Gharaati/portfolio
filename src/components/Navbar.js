be6a838336e78e8ff4ca5689203ad57eb49b809b
amir <amirgh1380@gmail.com>
2022-02-27 13:00:25 +0330

    navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,16 @@ const Navbar = () => {
     <header className="navbar">
       <div className="container">
         <div className="row flex-end">
-          <button type="button" className="navbar__toggler active">
+          <button type="button" className="navbar__toggler">
               <span></span>
           </button>
           <nav className="nav">
             <div className="nav__inner">
               <ul>
-                <li> <a href="#" className="nav__item">home</a></li>
-                <li> <a href="#" className="nav__item">about</a></li>
-                <li> <a href="#" className="nav__item">portfolio</a></li>
-                <li> <a href="#" className="nav__item">contact</a></li>
+                <li> <a href="#home" className="nav__item">home</a></li>
+                <li> <a href="#about" className="nav__item">about</a></li>
+                <li> <a href="#portfolio" className="nav__item">portfolio</a></li>
+                <li> <a href="#contact" className="nav__item">contact</a></li>
               </ul>
             </div>
           </nav>
