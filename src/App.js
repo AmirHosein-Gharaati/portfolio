2f463fd40bd62e559093c965ad4bac663361acc1
amir <amirgh1380@gmail.com>
2022-03-10 16:36:11 +0330

    initial commit for router

--- src/App.js
+++ src/App.js
@@ -6,18 +6,32 @@ import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import BgCircles from "./components/BgCircles";
 import PageLoader from "./components/PageLoader";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    document.onreadystatechange = () => {
+      setIsLoading(false);
+    };
+  });
+
   return (
     <div className="main">
-      <Navbar />
-      <BgCircles />
-      {/* <PageLoader /> */}
-      <Home />
-      {/* <About /> */}
-      {/* <Works /> */}
-      {/* <Project /> */}
-      {/* <Contact /> */}
+      <BrowserRouter>
+        <Navbar />
+        <BgCircles />
+        {isLoading && <PageLoader />}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/works/:id" element={<Project />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
