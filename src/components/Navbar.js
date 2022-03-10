69f1f22b4802027d770f47cd12d3a03eca32eab3
amir <amirgh1380@gmail.com>
2022-03-10 16:56:44 +0330

    added links to navbar buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,60 @@
-import { useState } from "react"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   const [navOpen, setNavOpen] = useState(false);
 
   const navbarClasses = () => {
-    if(navOpen){
-      return "nav"
-    } else{
-      return "nav v-hidden"
+    if (navOpen) {
+      return "nav";
+    } else {
+      return "nav v-hidden";
     }
-  }
-  
+  };
+
   return (
     <header className="navbar">
       <div className="container">
         <div className="row flex-end">
-          <button type="button" className="navbar__toggler" onClick={() => setNavOpen(!navOpen)}>
-              <span></span>
+          <button
+            type="button"
+            className="navbar__toggler"
+            onClick={() => setNavOpen(!navOpen)}
+          >
+            <span></span>
           </button>
           <nav className={navbarClasses()}>
             <div className="nav__inner">
               <ul>
-                <li> <a href="#home" className="nav__item">home</a></li>
-                <li> <a href="#about" className="nav__item">about</a></li>
-                <li> <a href="#portfolio" className="nav__item">portfolio</a></li>
-                <li> <a href="#contact" className="nav__item">contact</a></li>
+                <li>
+                  {" "}
+                  <Link to="/">
+                    <a className="nav__item">home</a>
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/about">
+                    <a className="nav__item">about</a>
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <a
+                    href="https://drive.google.com/file/d/1WTmP7O4shaqP0ZVCUHuFcHERulcZZTUv/view?usp=sharing"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="nav__item"
+                  >
+                    portfolio
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/contact">
+                    <a className="nav__item">contact</a>
+                  </Link>
+                </li>
               </ul>
             </div>
           </nav>
