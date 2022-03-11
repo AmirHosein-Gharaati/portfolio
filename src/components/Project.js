c69356fc5f33e194ae5c232d0e0416a4bcd797c0
amir <amirgh1380@gmail.com>
2022-03-11 21:01:10 +0330

    project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,50 +1,34 @@
+import { useParams } from "react-router-dom";
+import workData from "../data/data.json";
+
 const Project = () => {
+  const { id } = useParams();
+  const project = workData.filter((element) => element.id == id);
+  if (!project) {
+    // print 404
+  }
+
   return (
     <div className="project">
       <div className="project__inner">
         <div className="project__content">
           <div className="project__header">
             <div className="project__thumbnail">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
-                alt=""
-              />
+              <img src={project[0].imageUrl} alt="" />
             </div>
-            <h3>App landing page</h3>
+            <h3>{project[0].title}</h3>
           </div>
           <div className="project__body">
             <div className="description">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                blandit rutrum libero eget lobortis. Nunc nec velit lobortis,
-                ornare sem ut, fermentum turpis. Vivamus volutpat commodo lectus
-                id sagittis. Pellentesque sit amet auctor ex. Integer ultricies
-                felis ut sem rutrum viverra. Mauris a massa sit amet tortor
-                tincidunt vehicula. Nunc elementum nec ligula et tristique.
-                Etiam at fringilla dolor. Aliquam sed venenatis ex. Nam arcu
-                arcu, vehicula vel libero accumsan, iaculis rhoncus nisi. Donec
-                at neque felis. Sed vehicula dui sed tempor placerat.
-              </p>
+              <p>{project[0].text}</p>
             </div>
             <div className="project__general">
               <ul>
-                <li>
-                  Created - <span>4 Dec 2020</span>
-                </li>
-                <li>
-                  Technologies used - <span>HTML, CSS</span>
-                </li>
-                <li>
-                  Role - <span>Fronted</span>
-                </li>
-                <li>
-                  View Online{" "}
-                  <span>
-                    <a href="www.test.com" target="_blank">
-                      www.test.com
-                    </a>
-                  </span>
-                </li>
+                {Object.keys(project[0].metadata).map((key) => (
+                  <li key={key}>
+                    {key} - <span>{project[0].metadata[key]}</span>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
