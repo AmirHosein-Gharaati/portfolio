46b545b804bfaa0849741d8a39d1735097903bcc
amir <amirgh1380@gmail.com>
2022-03-12 10:57:37 +0330

    fixing some html base for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHamburger } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const location = useLocation();
@@ -16,37 +18,36 @@ const Navbar = () => {
 
   return (
     <header className="navbar">
-      <div className="container">
-        <div className="row">
-          <nav className="nav">
-            <div className="nav__logo">AG</div>
-            <div className="nav__items">
-              <ul>
-                <li>
-                  <Link className={getClasses("/")} to="/">
-                    home
-                  </Link>
-                </li>
-                <li>
-                  <Link className={getClasses("/about")} to="/about">
-                    about
-                  </Link>
-                </li>
-                <li>
-                  <Link className={getClasses("/works")} to="/works">
-                    Works
-                  </Link>
-                </li>
-                <li>
-                  <Link className={getClasses("/contact")} to="/contact">
-                    contact
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+      <nav className="nav">
+        <div className="nav__logo">AG</div>
+        <div className="nav__icon">
+          <FontAwesomeIcon icon={faHamburger} />
         </div>
-      </div>
+        <div className="nav__items">
+          <ul>
+            <li>
+              <Link className={getClasses("/")} to="/">
+                home
+              </Link>
+            </li>
+            <li>
+              <Link className={getClasses("/about")} to="/about">
+                about
+              </Link>
+            </li>
+            <li>
+              <Link className={getClasses("/works")} to="/works">
+                Works
+              </Link>
+            </li>
+            <li>
+              <Link className={getClasses("/contact")} to="/contact">
+                contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 };
