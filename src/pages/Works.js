9fb6222b128710c625ffbcd40480ce4a7fb2f6a2
amir <amirgh1380@gmail.com>
2022-03-13 15:45:37 +0330

    added Tilt in works page

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 import workData from "../data/data.json";
 import { changeTitle } from "../helper/title";
+import Tilt from "../components/Tilt";
 
 const Works = ({ title }) => {
   changeTitle(title);
   const data = workData;
+
+  const options = {
+    scale: 1.1,
+    speed: 1000,
+    max: 20,
+  };
 
   return (
     <section className="works__section">
@@ -17,18 +24,23 @@ const Works = ({ title }) => {
         <div className="works__content">
           {data.map((work) => {
             return (
-              <div className="works__item" key={work.id}>
+              <Tilt className="works__item" key={work.id} options={options}>
                 <div className="works__item__thumbnail">
-                  <img src={require("../assets/images/"+ work.imageUrl)} alt="" />
+                  <img
+                    src={require("../assets/images/" + work.imageUrl)}
+                    alt=""
+                  />
                 </div>
                 <h3 className="works__item__title">{work.title}</h3>
                 {work.metadata.Technologies && (
-                  <span className="works__item__meta">{work.metadata.Technologies}</span>
+                  <span className="works__item__meta">
+                    {work.metadata.Technologies}
+                  </span>
                 )}
                 <Link className="btn" to={work.id}>
                   view project
                 </Link>
-              </div>
+              </Tilt>
             );
           })}
         </div>
