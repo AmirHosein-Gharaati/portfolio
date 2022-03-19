1e57e5688e809f1c9750e8696b0aec029f9185c0
amir <amirgh1380@gmail.com>
2022-03-19 23:26:28 +0330

    functionality for projects with id if not found

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,46 +2,55 @@ import ReactMarkdown from "react-markdown";
 import { useParams } from "react-router-dom";
 import workData from "../data/data.json";
 import { changeTitle } from "../helper/title";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Project = () => {
   const { id } = useParams();
   const project = workData.filter((element) => element.id === id);
-  if (!project) {
-    // print 404
-  }
 
-  changeTitle(project[0].title);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (project.length !== 0) {
+      changeTitle(project[0].title);
+    } else {
+      navigate("/404");
+    }
+  });
 
   return (
-    <div className="project">
-      <div className="project__inner">
-        <div className="project__content">
-          <div className="project__header">
-            <div className="project__thumbnail">
-              <img
-                src={require("../assets/images/" + project[0].imageUrl)}
-                alt=""
-              />
+    project.length !== 0 && (
+      <div className="project">
+        <div className="project__inner">
+          <div className="project__content">
+            <div className="project__header">
+              <div className="project__thumbnail">
+                <img
+                  src={require("../assets/images/" + project[0].imageUrl)}
+                  alt=""
+                />
+              </div>
+              <h3 className="project__title">{project[0].title}</h3>
             </div>
-            <h3 className="project__title">{project[0].title}</h3>
-          </div>
-          <div className="project__body">
-            <div className="description">
-              <ReactMarkdown>{project[0].text}</ReactMarkdown>
-            </div>
-            <div className="project__general">
-              <ul>
-                {Object.keys(project[0].metadata).map((key) => (
-                  <li key={key}>
-                    {key} - <span>{project[0].metadata[key]}</span>
-                  </li>
-                ))}
-              </ul>
+            <div className="project__body">
+              <div className="description">
+                <ReactMarkdown>{project[0].text}</ReactMarkdown>
+              </div>
+              <div className="project__general">
+                <ul>
+                  {Object.keys(project[0].metadata).map((key) => (
+                    <li key={key}>
+                      {key} - <span>{project[0].metadata[key]}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
